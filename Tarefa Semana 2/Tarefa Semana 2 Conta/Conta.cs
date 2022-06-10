53fdc59f8d0520bb2f1853bc64d52eb056db25eb
Fabio Yamashita <yamashitafabio@gmail.com>
2022-06-10 16:11:27 -0300

    Adicionando a solução do exercício da Conta

--- Tarefa Semana 2/Tarefa Semana 2 Conta/Conta.cs
+++ Tarefa Semana 2/Tarefa Semana 2 Conta/Conta.cs
@@ -11,5 +11,72 @@ namespace Tarefa_Semana_2_Conta
         public int numConta;
         public double saldoConta;
         public double limiteConta;
+        public List<double> listaMovimentacoes;
+
+        public Conta(int numConta)
+        {
+            this.numConta = numConta;
+            this.listaMovimentacoes = new List<double>();
+        }
+        public Conta(int numConta, double saldoConta, double limiteConta) : this(numConta)
+        {
+            this.saldoConta = saldoConta;
+            this.limiteConta = limiteConta;
+        }
+
+        public void Deposita(double valorDeposito)
+        {
+            saldoConta += valorDeposito;
+            listaMovimentacoes.Add(valorDeposito);
+        }
+        public void Saca(double valorSaque)
+        {
+            if (valorSaque > saldoConta)
+            {
+                Console.WriteLine("Saldo insuficiente!");
+            }
+            else
+            {
+                saldoConta -= valorSaque;
+                listaMovimentacoes.Add(-valorSaque);
+            }
+        }
+        public void ImprimirExtrato()
+        {
+            // De padrão, imprimir todas as movimentações
+            this.ImprimirExtrato(listaMovimentacoes.Count);
+        }
+        public void ImprimirExtrato(int numMovimentacoes)
+        {
+            // Imprimindo as últimas n movimentações
+
+            if(numMovimentacoes > listaMovimentacoes.Count)
+            {
+                Console.WriteLine($"\nVocê não possui {numMovimentacoes} movimentações na conta!");
+                Console.WriteLine($"Vamos mostrar para você todas as {listaMovimentacoes.Count} movimentações da sua conta!");
+                numMovimentacoes = listaMovimentacoes.Count;
+            }
+
+            Console.WriteLine($"\n### SEU EXTRATO DAS ÚLTIMAS {numMovimentacoes} MOVIMENTAÇÕES ###");
+            for (int i = listaMovimentacoes.Count - 1; i >= 0; i--)
+            {
+                if(listaMovimentacoes[i] > 0)
+                {
+                    Console.WriteLine($"#{i+1} Depósito: +R$ {listaMovimentacoes[i].ToString("N2")}");
+                }
+                else
+                {
+                    Console.WriteLine($"#{i+1} Saque: -R$ {Math.Abs(listaMovimentacoes[i]).ToString("N2")}");
+                }
+            }
+            Console.WriteLine($"-----------------------------------------------");
+        }
+        public void ConsultaSaldo()
+        {
+            Console.WriteLine("\n### CONSULTA SALDO ###");
+            Console.WriteLine($"Conta: {numConta}");
+            Console.WriteLine($"Saldo: R$ {saldoConta.ToString("N2")}");
+            Console.WriteLine("------------------------");
+        }
     }
 }
--- Tarefa Semana 2/Tarefa Semana 2 Conta/TestaMetodosConta.cs
+++ Tarefa Semana 2/Tarefa Semana 2 Conta/TestaMetodosConta.cs
@@ -10,7 +10,32 @@ namespace Tarefa_Semana_2_Conta
     {
         static void Main(string[] args)
         {
+            // Criando diferentes contas com diferentes construtores
+            Conta c1 = new Conta(15065, 0, 1500);
+            Conta c2 = new Conta(10500);
 
+            // Testando com a conta c1
+            c1.Deposita(150);
+            c1.Deposita(1000);
+            c1.Saca(100);
+            c1.Saca(500);
+            c1.Saca(500);
+            c1.Deposita(500);
+            c1.Deposita(50);
+            c1.Deposita(10);
+
+            c1.ConsultaSaldo();
+
+            c1.ImprimirExtrato(10);
+
+            // Testando com a conta c2
+            c2.Deposita(150);
+            c2.Deposita(1000);
+            c2.Saca(1000);
+
+            c2.ConsultaSaldo();
+
+            c2.ImprimirExtrato();
         }
     }
 }
